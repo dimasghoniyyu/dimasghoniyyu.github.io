3d54d03935bb07bb0bebd40fd8e978f3826b4622
dimasghoniyyu <ghoniyyudimas@gmail.com>
2017-06-20 14:18:43 +0700

    Add files via upload

---
+++ UASweb/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+		"question": "  4 + 6=",
+		"option1": "10",
+		"option2": "2",
+		"option3": "24",
+		"option4": "5",
+		"answer": "1"
+}, {
+		"question": "  8 + 4 * 5 - 6 / 3 =",
+		"option1": "16",
+		"option2": "26",
+		"option3": "20",
+		"option4": "14",
+		"answer": "2"
+}, {
+		"question": "  Jika volume kubus 312 cm3 maka berpakah masing-masing sisinya?",
+		"option1": "16",
+		"option2": "8",
+		"option3": "4",
+		"option4": "6",
+		"answer": "2"
+}, {
+		"question": "  Jika luas segitiga 24 cm2 dan memiliki panjang 8 cm berapa tinggi segitiga?",
+		"option1": "10",
+		"option2": "3",
+		"option3": "8",
+		"option4": "6",
+		"answer": "4"
+}, {
+		"question": "  Berpakah keliling lingkaran jika memiliki r = 28 cm ?",
+		"option1": "8pi",
+		"option2": "14pi",
+		"option3": "7pi",
+		"option4": "21pi",
+		"answer": "2"
+}, {
+		"question": "Berpakah luas lingkaran jika memiliki d = 14 cm, pi = 22/7",
+		"option1": "154",
+		"option2": "164",
+		"option3": "144",
+		"option4": "200",
+		"answer": "1"
+},{
+		"question": "Berapakah sisi kubus",
+		"option1": "6",
+		"option2": "14",
+		"option3": "12",
+		"option4": "8",
+		"answer": "3"
+}, {
+		"question": "  Berapakah sudut limas segitiga",
+		"option1": "3",
+		"option2": "8",
+		"option3": "6",
+		"option4": "4",
+		"answer": "4"
+}, {
+		"question": " Berapakah sisi miring segitiga siku-siku jika memiliki; alas = 6 cm, tinggi = 8 cm ?",
+		"option1": "10",
+		"option2": "12",
+		"option3": "9",
+		"option4": "6",
+		"answer": "1"
+}, {
+		"question": " Berapakah sisi miring segitiga siku-siku jika memiliki; alas = 8 cm, tinggi = 8 cm",
+		"option1": "8",
+		"option2": "7",
+		"option3": "8 akar2",
+		"option4": "8 akar3",
+		"answer": "3"
+}]
